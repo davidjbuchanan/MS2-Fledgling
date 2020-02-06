0bd155ba30a0e69350eed13539e2f2238bef1127
davidjbuchanan <davidjbuchanan@gmail.com>
2020-02-06 18:11:42 +0000

    styling to allow responsiveness on expanding and contracting display port. README.md updated to incorporate mockup jpeg. All files 'cleaned' to remove developer's comments.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,20 +1,3 @@
-/*function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 55.8597, lng: -4.2550}  
-    });
-}*/
-
-
-/*center: {lat: 55.8636, lng: -4.2825} Finnieston*/
-
-/*center: {lat: 55.8772, lng: -4.2904} westend*/
-
-/*center: {lat: 55.8597, lng: -4.2300} eastend*/
-
-/*center: {lat: 55.8588, lng: -4.2489} merchant city needs a zoom in*/
-
-/*center: {lat: 55.8430, lng: -4.2636} southside needs a zoom out*/
 var map;
 var centre = {lat: 55.8597, lng: -4.2550};
 var finn = {lat: 55.8636, lng: -4.2825};
@@ -27,12 +10,10 @@ function initMap() {
     var myOptions = {
       zoom: 15,
       center: centre
-      /*mapTypeId: google.maps.MapTypeId.ROADMAP*/
     };  
     map = new google.maps.Map(document.getElementById('map'), myOptions);
-}
+};
 
 function changeCenter(center) {
       map.setCenter(center);
-      /*marker.setPosition(center);*/
-    }
+};
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,66 +1,42 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "confirmation_of_receipt_fledgling", {
+    var fields = {
         "from_name": contactForm.name.value, 
         "from_email": contactForm.emailaddress.value, 
         "employment_status": contactForm.employmentstatus.value, 
-
-        function hood1() {
-            var checkBox = document.getElementById("check1");
-            if (checkBox.checked == true){
-                "neighbourhood_interest1": contactForm.neighbourhood1.value,
-            } 
-        }, 
-
-        function hood2() {
-            var checkBox = document.getElementById("check2");
-            if (checkBox.checked == true){
-                "neighbourhood_interest2": contactForm.neighbourhood2.value,
-            } 
-        }, 
-
-        function hood3() {
-            var checkBox = document.getElementById("check3");
-            if (checkBox.checked == true){
-                "neighbourhood_interest3": contactForm.neighbourhood3.value,
-            } 
-        }, 
-
-        function hood4() {
-            var checkBox = document.getElementById("check4");
-            if (checkBox.checked == true){
-                "neighbourhood_interest4": contactForm.neighbourhood4.value,
-            } 
-        }, 
-
-        function hood5() {
-            var checkBox = document.getElementById("check5");
-            if (checkBox.checked == true){
-                "neighbourhood_interest5": contactForm.neighbourhood5.value,
-            } 
-        }, 
-
-        function hood6() {
-            var checkBox = document.getElementById("check6");
-            if (checkBox.checked == true){
-                "neighbourhood_interest6": contactForm.neighbourhood6.value,
-            } 
-        }, 
-
-        /*
-        "neighbourhood_interest1": contactForm.neighbourhood1.value,
-        "neighbourhood_interest2": contactForm.neighbourhood2.value,
-        "neighbourhood_interest3": contactForm.neighbourhood3.value,
-        "neighbourhood_interest4": contactForm.neighbourhood4.value,
-        "neighbourhood_interest5": contactForm.neighbourhood5.value,
-        "neighbourhood_interest6": contactForm.neighbourhood6.value*/
-    })
+    };
+    var checkBox = document.getElementById("check1");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest1"] = contactForm.neighbourhood1.value;
+    };
+    var checkBox = document.getElementById("check2");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest2"] = contactForm.neighbourhood2.value;
+    };
+    var checkBox = document.getElementById("check3");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest3"] = contactForm.neighbourhood3.value;
+    };
+    var checkBox = document.getElementById("check4");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest4"] = contactForm.neighbourhood4.value;
+    };
+    var checkBox = document.getElementById("check5");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest5"] = contactForm.neighbourhood5.value;
+    };
+    var checkBox = document.getElementById("check6");
+    if (checkBox.checked == true){
+        fields["neighbourhood_interest6"] = contactForm.neighbourhood6.value;
+    };
+    
+    emailjs.send("gmail", "confirmation_of_receipt_fledgling", fields)
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+            console.log("SUCCESS", response, fields);
         },
         function(error) {
             console.log("FAILED", error);
         }
     );
-    return false;  // To block from loading a new page
+    return false; 
 }
